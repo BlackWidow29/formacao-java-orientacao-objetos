6fcfb5df8305f10ef2356948f942e30308407847
Isabelly Dias <isabellydias29@gmail.com>
2021-07-27 22:20:39 -0300

    topico 5 - A classe object

--- java-e-java-lang-programe-com-a-classe-object-e-string/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ java-e-java-lang-programe-com-a-classe-object-e-string/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -14,7 +14,7 @@ public abstract class Conta {
      * @param agencia
      * @param numero
      */
-    public Conta(int agencia, int numero){
+    public Conta(int agencia, int numero) {
         Conta.total++;
         //System.out.println("O total de contas é " + Conta.total);
         this.agencia = agencia;
@@ -27,61 +27,66 @@ public abstract class Conta {
 
     /**
      * Valor precisa ser maior que o saldo
+     *
      * @param valor
      * @throws SaldoInsuficienteException
      */
-    public void saca(double valor) throws SaldoInsuficienteException{
-    	
-        if(this.saldo < valor) {
+    public void saca(double valor) throws SaldoInsuficienteException {
+
+        if (this.saldo < valor) {
             throw new SaldoInsuficienteException("Saldo: " + this.saldo + ", Valor: " + valor);
-        } 
-        
-        this.saldo -= valor;       
+        }
+
+        this.saldo -= valor;
     }
 
-    public void transfere(double valor, Conta destino) throws SaldoInsuficienteException{
+    public void transfere(double valor, Conta destino) throws SaldoInsuficienteException {
         this.saca(valor);
         destino.deposita(valor);
     }
 
-    public double getSaldo(){
+    public double getSaldo() {
         return this.saldo;
     }
 
-    public int getNumero(){
+    public int getNumero() {
         return this.numero;
     }
 
-    public void setNumero(int numero){
-        if(numero <= 0) {
+    public void setNumero(int numero) {
+        if (numero <= 0) {
             System.out.println("Nao pode valor menor igual a 0");
             return;
         }
         this.numero = numero;
     }
 
-    public int getAgencia(){
+    public int getAgencia() {
         return this.agencia;
     }
 
-    public void setAgencia(int agencia){
-       if(agencia <= 0) {
-           System.out.println("Nao pode valor menor igual a 0");
-           return;
-       }
-       this.agencia = agencia;
+    public void setAgencia(int agencia) {
+        if (agencia <= 0) {
+            System.out.println("Nao pode valor menor igual a 0");
+            return;
+        }
+        this.agencia = agencia;
     }
 
-    public void setTitular(Cliente titular){
+    public void setTitular(Cliente titular) {
         this.titular = titular;
     }
 
-    public Cliente getTitular(){
+    public Cliente getTitular() {
         return this.titular;
     }
 
-    public static int getTotal(){
+    public static int getTotal() {
         return Conta.total;
     }
 
+    @Override
+    public String toString() {
+        return "Numero: " + this.numero + ", Agencia: " + this.agencia;
+    }
 }
